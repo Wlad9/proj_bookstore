08f0c9dc64699039117610abb157f67532cf4b3c
wlad9 <zav8b9@gmail.com>
2022-09-21 10:35:19 -0300

    Primeiro commit: estrutura do projeto

---
+++ src/main/java/com/asdrubal/projbookstoregerente/ProjBookstoreGerenteApplication.java
@@ -0,0 +1,13 @@
+package com.asdrubal.projbookstoregerente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjBookstoreGerenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjBookstoreGerenteApplication.class, args);
+	}
+
+}
